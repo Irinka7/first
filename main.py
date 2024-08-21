75c6f526d81f26caca74d3361aaa4604b3495ebe
Irinka7 <7688360@mail.ru>
2024-08-21 10:50:13 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+print("Hello world")
+#1st 'program'
+print(9 ** 0.5 * 5)
+#2nd 'programm'
+print(9.99 > 9.98 and 1000 != 1000.1)
+#3nd 'program'
+print(2 * 2+ 2)
+print(2 * (2 + 2))
+print((2 * 2+ 2) == (2 * (2 + 2)))
+#4nd 'program'
+print(int(((float('123.456') - 123) * 10)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
